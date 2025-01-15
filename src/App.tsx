9df78caf927473ec9e25653ab9f4f948ab739128
LinaGoebel <galina283@freenet.de>
2025-01-15 19:31:32 +0100

    add homework 16

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-
+// import Layout from "./pages/UsersApp/Layout/Layout"
+// import Home from "./pages/UsersApp/Home/Home"
+// import Users from "./pages/UsersApp/Users/Users"
 
 //lessons
-// import Lessons16 from "./lessons/Lessons16/Lessons16"
-import Layout from "./pages/UserApp/Layout/Layout"
-import Home from "./pages/UserApp/Home/Home"
-import Users from "./pages/UserApp/Users/Users"
+// import Lesson16 from "./lessons/Lesson16/Lesson16"
 
 //homeworks
+import Homework16 from "./homeworks/Homework16/Homework16"
 
-//consultation
-// import Consultation08 from "./consultation/Consultation08/Consultation08"
+//consultations
+// import Consultation08 from "./consultations/Consultation08/Consultation08"
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* <Consultation08 /> */}
-      {/* <Lessons16 /> */}
-      {/*UserApp - Practice - Lesson16 */}
-      <Layout>
+      {/* <Lesson16 /> */}
+      {/* UserApp - Practice - Lesson16 */}
+      {/* <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="*" element='Page not found' />
+          <Route path='/' element={<Home/>}/>
+          <Route path='/users' element={<Users/>}/>
+          <Route path='*' element='Page not found'/>
         </Routes>
-      </Layout>
+      </Layout> */}
+      <Homework16 />
     </BrowserRouter>
   )
 }
